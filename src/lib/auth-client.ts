e11b6da73f2f3d7858590eb82427fdd35ecac158
graslowsnail <graslowsnail@gmail.com>
2025-06-19 19:28:46 -0400

    chagne prod url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NODE_ENV === "production" ? "https://sample-app" : "http://localhost:3000"
+    baseURL: process.env.NODE_ENV === "production" ? "https://virtual-museum-ten.vercel.app" : "http://localhost:3000"
 });
 
 export const {
